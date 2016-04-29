4d5e0498b2ac280709ac5053bd8645f6cfc380af
Morten <morteano@stud.ntnu.no>
2016-04-29 16:08:20 +0200

    Added the NSGA II-algorithm

--- Assignment5/mtsp.py
+++ Assignment5/mtsp.py
@@ -3,7 +3,7 @@ import numpy as np
 from random import randint
 import math
 import matplotlib.pyplot as plt
-
+import operator
 
 # a class containing the cost and distance matrices
 class Data:
@@ -38,6 +38,10 @@ class EaType:
         self.genotype = self.makeGenotype()
         self.phenotype = self.genotype
         self.dist, self.cost = self.fitness(data)
+        self.rank = None
+        self.n = None
+        self.dominates = []
+        self.crowdDist = 0
 
     def makeGenotype(self):
         genotype = []
@@ -95,13 +99,85 @@ class Population:
                 return False
         return True
 
+    def nonDomSort(self):
+        F = []
+        F0 = []
+        for eaType in self.population:
+            eaType.dominates = []
+            eaType.n = 0
+            for otherEaType in self.population:
+                if eaType.dist < otherEaType.dist and eaType.cost < otherEaType.cost:
+                    eaType.dominates.append(otherEaType)
+                elif eaType.dist > otherEaType.dist and eaType.cost > otherEaType.cost:
+                    eaType.n += 1
+            if eaType.n == 0:
+                eaType.rank = 1
+                F0.append(eaType)
+        F.append(F0)
+        i = 1
+        while len(F[i-1]) > 0:
+            Q = []
+            for eaType in F[i-1]:
+                for otherEaType in eaType.dominates:
+                    otherEaType.n -= 1
+                    if otherEaType.n == 0:
+                        otherEaType.rank = i + 1
+                        Q.append(otherEaType)
+            i += 1
+            F.append(Q)
+        F.pop(-1)
+        return F
+
+    def crowdingDist(self, I):
+        for eaType in I:
+            eaType.crowdDist = 0
+        for obj in ['dist', 'cost']:
+            sortedF = sorted(I, key=operator.attrgetter(obj))
+            sortedF[0].crowdDist = float("inf")
+            sortedF[-1].crowdDist = float("inf")
+            for i in range(1, len(I)-1):
+                if obj == 'dist':
+                    sortedF[i].crowdDist += (sortedF[i+1].dist-sortedF[i-1].dist)/(sortedF[-1].dist-sortedF[0].dist)
+                elif obj == 'cost':
+                    sortedF[i].crowdDist += (sortedF[i+1].cost-sortedF[i-1].cost)/(sortedF[-1].cost-sortedF[0].cost)
+
 
 def main():
     data = Data()
-    population = Population(data, 100)
+    population = Population(data, 10)
     distEa, costEa = population.bestFitness()
     print(distEa.dist, distEa.cost)
     print(costEa.dist, costEa.cost)
+    F = population.nonDomSort()
+    print("Before")
+    for type in F[0]:
+        print(type.crowdDist)
+    population.crowdingDist(F)
+    print("After")
+    for type in F[0]:
+        print(type.crowdDist)
     population.plotPopulation()
 
-main()
+def tempMain():
+    N = 10
+    data = Data()
+    population = Population(data, N)
+    nextGen = Population(data, N)
+    for i in range(10):
+        population.population += nextGen.population
+        F = population.nonDomSort()
+        population = Population(data, 0)
+        i = 1
+        while len(population.population)+len(F[i-1]) < N:
+            population.crowdingDist(F[i-1])
+            population.population += F[i-1]
+            i += 1
+        sortedF = sorted(F[i-1], key=operator.attrgetter('crowdDist'))
+        j = 0
+        while len(population.population) < N:
+            population.population.append(sortedF[j])
+            j += 1
+        population.plotPopulation()
+        nextGen = Population(data, N)
+
+tempMain()
